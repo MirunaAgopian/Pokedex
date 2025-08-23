ad5981a9c48f1d6ac9fcdb1b575fefb550cebfd6
Miruna Agopian <agopian.miruna@yahoo.com>
2025-08-23 19:40:54 +0200

    refractored setPokemonImgColor()

--- scripts/helpers/domHelpers.js
+++ scripts/helpers/domHelpers.js
@@ -4,40 +4,7 @@ function setPokemonImgColor(pokemonDetails) {
     const type = pokemonDetails.types[0].type.name;
     const number = pokemonDetails.id;
     let pokeImg = document.getElementById(`pokemon_${number}`);
-
-    if(type === 'grass'){
-        pokeImg.classList.add('grass');
-    } else if(type === 'fire') {
-        pokeImg.classList.add('fire');
-    } else if(type === 'water'){
-        pokeImg.classList.add('water');
-    } else if(type === 'bug'){
-        pokeImg.classList.add('bug');
-    } else if(type === 'normal'){
-        pokeImg.classList.add('normal');
-    } else if(type === 'poison'){
-        pokeImg.classList.add('poison');
-    } else if(type === 'electric'){
-        pokeImg.classList.add('electric');
-    } else if(type === 'ground'){
-        pokeImg.classList.add('ground');
-    } else if(type === 'fairy'){
-        pokeImg.classList.add('fairy');
-    } else if(type === 'fighting'){
-        pokeImg.classList.add('fighting');
-    } else if(type === 'psychic'){
-        pokeImg.classList.add('psychic');
-    } else if(type === 'rock'){
-        pokeImg.classList.add('rock');
-    } else if(type === 'ghost'){
-        pokeImg.classList.add('ghost');
-    } else if(type === 'ice'){
-        pokeImg.classList.add('ice');
-    } else if(type === 'dragon'){
-        pokeImg.classList.add('dragon');
-    } else if(type === 'dark'){
-        pokeImg.classList.add('dark');
-    }
+    pokeImg.classList.add(type); //this is a really good refractor!
 }
 
 function setSinglePokemonImgColor(pokemonDetails) {
